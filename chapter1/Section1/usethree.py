26039149b244af71283d3c1dfe4bf384aff42722
Lan.Truong <cheungwinglun1989@gmail.com>
2016-12-01 15:47:10 +0700

    Adding usethree.py

---
+++ chapter1/Section1/usethree.py
@@ -0,0 +1,6 @@
+#take three input names and print out on console
+person1 = raw_input("Enter first name: ")
+person2 = raw_input("Enter first name: ")
+person3 = raw_input("Enter first name: ")
+
+print "Hi " + person1 + ", " + person2 + ", " + person3 + "."
