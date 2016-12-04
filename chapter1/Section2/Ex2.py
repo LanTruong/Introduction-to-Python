0f96d66c8356d757ee65aa0fed22b392d55a1ea6
Lan.Truong <cheungwinglun1989@gmail.com>
2016-12-04 16:06:04 +0700

    Modify Ex2.py

--- chapter1/Section2/Ex2.py
+++ chapter1/Section2/Ex2.py
@@ -1,6 +1,8 @@
 ##prompt argument and verify sin^2 + cos^2 =1
 import math
 theta = float(raw_input("input an argument: "))
+value = math.sin(theta)**2 + math.cos(theta)**2
+print "true value: " + str(value)
 if math.fabs((math.sin(theta)**2 + math.cos(theta)**2 - 1.0)) < math.exp(-20):
     print "sin^2 + cos^2 = 1 is right"
 else:
